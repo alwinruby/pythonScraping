47bed5a7280aa9dc0ac4d52c5675fbd2f08da6e3
alwinruby <alwinsolanky@gmail.com>
2021-06-16 21:24:13 +0100

    Quit driver

--- scraping_beginners.py
+++ scraping_beginners.py
@@ -2,9 +2,17 @@ from selenium import webdriver
 import time
 
 # Writing our First Selenium Python Test
-web = 'https://sports.tipico.de/en/all/football/spain/la-liga'  # you can choose any other league (update 1)
+# web = 'https://sports.tipico.de/en/all/football/spain/la-liga'  # choose any other league
+web = 'https://sports.tipico.de/en/all/football/england'
 path = '/usr/local/bin/chromedriver'
-
 
 driver = webdriver.Chrome(path)
 driver.get(web)
+
+#Make ChromeDriver click a button
+time.sleep(5) #add implicit wait, if necessary
+accept = driver.find_element_by_xpath('//*[@id="_evidon-accept-button"]')
+accept.click()
+
+time.sleep(5) #add implicit wait, if necessary
+driver.quit()
